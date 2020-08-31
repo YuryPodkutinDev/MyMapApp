fc35710bea286341caf776bdadbfaf566293eb66
YuryPodkutin <yurapodkutin@yandex.ru>
2020-08-31 22:26:13 +0700

    Create app

---
+++ MyMapApp/MyMapApp/App/Flow/Map/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  MyMapApp
+//
+//  Created by Yury Podkutin on 31.08.2020.
+//  Copyright © 2020 Yury Podkutin. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
